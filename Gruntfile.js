e4f0714878c0bb62aa28bf68d7da4a70362c01a8
Pedro Nauck <pedronauck@gmail.com>
2014-03-09 03:52:25 -0300

    add base project structure with assemble.io

---
+++ Gruntfile.js
@@ -0,0 +1,91 @@
+'use strict';
+module.exports = function(grunt) {
+  require('time-grunt')(grunt);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    site: grunt.file.readYAML('_config.yml'),
+
+    watch: {
+      assemble: {
+        files: [
+          '<%= site.app %>/{layouts,partials,pages}/{,*/}*.{md,hbs,yml}'
+        ],
+        tasks: ['assemble']
+      },
+      livereload: {
+        options: { livereload: true },
+        files: [
+          '<%= site.dist %>/{,*/}*.html',
+          '<%= site.dist %>/{,*/}*.css',
+          '<%= site.dist %>/{,*/}*.js',
+          '<%= site.dist %>/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      }
+    },
+
+    connect: {
+      options: {
+        port: 9000,
+        livereload: 35729,
+        // change this to '0.0.0.0' to access the server from outside
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          open: true,
+          base: [
+            '<%= site.dist %>'
+          ]
+        }
+      }
+    },
+
+    assemble: {
+      pages: {
+        options: {
+          flatten: true,
+          assets: '<%= site.assets %>',
+          layout: '<%= site.layouts %>/default.hbs',
+          partials: '<%= site.partials %>/**/*.hbs'
+        },
+        files: {
+          '<%= site.dist %>/': ['<%= site.app %>/pages/*.hbs']
+        }
+      }
+    },
+
+    // Before generating any new files,
+    // remove any previously-created files.
+    clean: ['<%= site.dist %>/**/*.{html,xml}'],
+
+    compass: {
+      dist: {
+        options: { config: './config.rb' }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('assemble');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+
+  grunt.registerTask('server', [
+    'clean',
+    'assemble',
+    'connect:livereload',
+    'watch'
+  ]);
+
+  grunt.registerTask('build', [
+    'clean',
+    'assemble',
+    'compass:dist'
+  ]);
+
+  grunt.registerTask('default', [
+    'build'
+  ]);
+};
---
+++ config.rb
@@ -0,0 +1,9 @@
+http_path = "/"
+css_dir = "dist/css"
+sass_dir = "src/assets/stylesheets"
+images_dir = "src/assets/images"
+javascripts_dir = "src/assets/javascripts"
+
+output_style = :compressed
+relative_assets = true
+line_comments = false
