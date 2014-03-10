f987d1cf273865cababbf22c60f17dd7d3971258
Pedro Nauck <pedronauck@gmail.com>
2014-03-10 13:37:49 -0300

    add some grunt tasks (concat, uglify, imagemin, copy)

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,6 +13,10 @@ module.exports = function(grunt) {
         ],
         tasks: ['assemble']
       },
+      js: {
+        files: '<%= site.js %>/**/*.js',
+        tasks: ['concat', 'uglify']
+      },
       compass: {
         files: ['<%= site.sass%>/**/*.{scss,sass,css}'],
         tasks: ['compass']
@@ -32,7 +36,6 @@ module.exports = function(grunt) {
       options: {
         port: 9000,
         livereload: 35729,
-        // change this to '0.0.0.0' to access the server from outside
         hostname: 'localhost'
       },
       livereload: {
@@ -51,7 +54,11 @@ module.exports = function(grunt) {
           flatten: true,
           assets: '<%= site.assets %>',
           layout: '<%= site.layouts %>/default.hbs',
-          partials: '<%= site.partials %>/**/*.hbs'
+          partials: '<%= site.partials %>/**/*.hbs',
+          plugins: ['assemble-contrib-permalinks'],
+          permalinks: {
+            structure: ':basename/index.html'
+          }
         },
         files: {
           '<%= site.dist %>/': ['<%= site.app %>/pages/*.hbs']
@@ -59,9 +66,52 @@ module.exports = function(grunt) {
       }
     },
 
-    // Before generating any new files,
-    // remove any previously-created files.
-    clean: ['<%= site.dist %>/**/*.{html,xml}'],
+    clean: [
+      '<%= site.dist %>/**/*.{html,xml,css,png,jpg,gif,svg,js}'
+    ],
+
+    // concatenate scripts
+    concat: {
+      dist: {
+        options: { separator: ";\n" },
+        files: {
+          '<%= site.dist %>/js/application.js': ['<%= site.js %>/**/*.js']
+        }
+      }
+    },
+
+    // minification the javascripts scripts
+    uglify: {
+      dist: {
+        files: {
+          '<%= site.dist %>/js/application.js': [
+            '<%= site.dist %>/js/application.js'
+          ]
+        }
+      }
+    },
+
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: '<%= site.dist %>/images',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: '<%= site.dist %>/images'
+        }]
+      }
+    },
+
+    copy: {
+      main: {
+        files: [{
+          expand: true,
+          cwd: '<%= site.images %>',
+          src: '**/*',
+          dest: '<%= site.dist %>/images'
+        }]
+      }
+    },
 
     compass: {
       dist: {
@@ -75,21 +125,36 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('jsmin', [
+    'concat',
+    'uglify'
+  ])
+
+  grunt.registerTask('build', [
+    'clean',
+    'compass:dist',
+    'jsmin',
+    'assemble',
+    'copy',
+    'imagemin'
+  ]);
 
   grunt.registerTask('server', [
     'clean',
+    'compass:dist',
+    'jsmin',
     'assemble',
+    'copy',
     'connect:livereload',
     'watch'
   ]);
 
-  grunt.registerTask('build', [
-    'clean',
-    'assemble',
-    'compass:dist'
-  ]);
-
   grunt.registerTask('default', [
-    'build'
+    'server'
   ]);
 };
